148f877815032e878b68faa6d8a556480f99f4eb
Alice Brunel <alc.brunel@gmail.com>
2019-09-17 11:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,14 @@ class Student
   end
   
   def self.all 
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
   
 end
